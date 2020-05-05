38766534cfb2604871d51e950d87e5f66aee9dca
Langster2323 <langahkeem@gmail.com>
2020-05-05 14:36:36 -0500

    Updated delete button. Fixed bugs.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService'
+import { getMovies, deleteMovie } from '../services/fakeMovieService'
 
 class Movies extends Component {
     state = { 
@@ -12,34 +12,40 @@ class Movies extends Component {
         })
       }
     
-      handleTotalMovie = (movies) => {
-        const movieTotal = this.state.movies.length;
+      handleTotalMovie = () => {
+        const { movies } = this.state;
+        const movieTotal = movies.length;
         return movieTotal
+      }
+
+      handleDelete = (movie) => {
+        console.log(movie);
+        
       }
       render() {
         const { movies } = this.state
         return (
-          <main class="container">
+          <main className="container">
             <h1>Hello World</h1>
             <h3>There are {this.handleTotalMovie()} movies</h3>
-            <table>
+            <table className="table">
               <thead>
                 <tr>
-                  <td>Title</td>
-                  <td>Genre</td>
-                  <td>Stock</td>
-                  <td>Rate</td>
+                  <th className="col">Title</th>
+                  <th className="col">Genre</th>
+                  <th className="col">Stock</th>
+                  <th className="col">Rate</th>
                 </tr>
               </thead>
               <tbody>
                 {movies.map((movie, index) => {
                   return (
-                    <tr>
+                    <tr key={index}>
                       <td>{movie.title}</td>
                       <td>{movie.genre.name}</td>
                       <td>{movie.numberInStock}</td>
                       <td>{movie.dailyRentalRate}</td>
-                      <td><button>Delete</button></td>
+                      <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>
                   )
                 })}
