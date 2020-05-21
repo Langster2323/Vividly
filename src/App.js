0327b478a69b032f93eb517b5bff4659d57ee5a5
Langster2323 <langahkeem@gmail.com>
2020-05-21 08:57:41 -0500

    implementing react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import Movies from './components/movies';
+import NavBar from './components/navBar';
+import Customers  from './components/customers';
+import Rentals from './components/rentals'
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
 
 function App() {
   return (
     <React.Fragment>
-      <Movies />
+      <NavBar />
+      <Switch>
+        <Route path="/movies/:id" component={MovieForm} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect exact from="/" to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </React.Fragment>
   );
 }
