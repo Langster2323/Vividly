12dacd0c49893d9ec30afb6635b5003079907a08
Langster2323 <langahkeem@gmail.com>
2020-05-21 10:03:56 -0500

    basic validation

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Input = ({ name, value, label, onChange }) => {
+const Input = ({ name, value, label, onChange, error }) => {
     return ( 
         <div className="form-group">
             <label htmlFor={name}>{ label }</label>
@@ -11,6 +11,7 @@ const Input = ({ name, value, label, onChange }) => {
             value={value}
             id={name} 
             type="text" />
+            {error && <div className="alert alert-danger">{error}</div>}
         </div>
 );
 }
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,24 +19,37 @@ class LoginForm extends Component {
         return Object.keys(errors).length === 0 ? null : errors;
     }
 
+    validateProperty = ({ name, value }) => {
+        if (name === 'username') {
+            if (value.trim() === '') return 'Username is required.';
+        }
+        if (name === 'password') {
+            if (value.trim() === '') return 'Password is required.';
+        }
+    }
+
     handleChange = ({currentTarget: input}) => {
+        const errors = {...this.state.errors}
+        const errorMessage = this.validateProperty(input)
+        if(errorMessage) errors[input.name] = errorMessage
+        else delete errors[input.name]
+
         const account = { ...this.state.account }
         account[input.name] = input.value;
-        this.setState({ account })
+        this.setState({ account, errors })
     }
 
     handleSubmit = e => {
         e.preventDefault();
 
         const errors = this.validate();
-        console.log(errors);
         
-        this.setState({ errors })
+        this.setState({ errors: errors || {} })
         if (errors) return
         
     }
     render() { 
-        const { account } = this.state;
+        const { account, errors } = this.state;
         return ( 
             <div>
                 <h1>Login</h1>
@@ -45,12 +58,14 @@ class LoginForm extends Component {
                     name="username" 
                     value={account.username} 
                     label="Username" 
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                    error={errors.username} />
                     <Input 
                     name="password" 
                     value={account.password} 
                     label="Password" 
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                    error={errors.password} />
                     
                     <button className="btn btn-primary">Login</button>
                 </form>
